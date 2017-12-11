3ccd7927417c1c54446ceccb1deea449593248af
Коберский Мирослав <mir-os@mail.ru>
2017-12-11 23:03:18 +1000

    Fixed critical coniditions bug

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import fdb
 from math import ceil
 from flask import Flask
 from flask import render_template
+from flask import abort
 from werkzeug.urls import url_encode
 from query import *
 
@@ -18,10 +19,14 @@ class Paging:
         self.cur = cur
 
     def nextInit(self, query, params):
+        print('----------ROWS QUERY---------------')
+        print(query)
+        print(params)
+        print('----------ROWS QUERY---------------')
         self.cur.execute(query, params)
         self.rowsNum = int(self.cur.fetchall()[0][0])
-        self.OnPage = min(10000,max(1,request.args.get('onpage', 5, type=int)))
-        self.pagesNum = int(ceil(self.rowsNum / self.OnPage))
+        self.onPage = min(10000,max(1,request.args.get('onpage', 5, type=int)))
+        self.pagesNum = int(ceil(self.rowsNum / self.onPage))
         self.page = request.args.get('page', 0, type=int)
         if not self.page in range(self.pagesNum):
             self.page = 0
@@ -32,13 +37,13 @@ def changeArg(arg, val):
     args[arg] = val
     return '{}?{}'.format(request.path, url_encode(args))
 
-@app.template_global()
-def getModifyPageUrl(table, id):
-    args = {}
-    args['t'] = table
-    args['id'] = id
-    return '/modify' + '{}?{}'.format(request.path, url_encode(args))
-
+def getMeta(table):
+    meta = []
+    for field in table.__dict__:
+        attr = getattr(table, field)
+        if isinstance(attr, metadata.BaseField) or isinstance(attr, metadata.RefField):
+            meta.append(getattr(table, field))
+    return meta
 
 @app.route("/")
 def home():
@@ -49,19 +54,19 @@ def home():
     charset='UTF-8'
     )
     cur = con.cursor()
-    meta = []
     selected_table = request.args.get('t', '')
     if selected_table in tables:
         selected_table = getattr(metadata,selected_table)
     else:
         selected_table = getattr(metadata, tables[0])
-    for field in selected_table.__dict__:
-        attr = getattr(selected_table,field)
-        if isinstance(attr,metadata.BaseField) or isinstance(attr, metadata.RefField):
-            meta.append(getattr(selected_table,field))
+    meta = getMeta(selected_table)
     search = Search(selected_table)
     paging = Paging(cur)
-    query = QueryBuilder(selected_table, meta, search, paging).query
+    query = QueryBuilder.getTableView(QueryBuilder(),selected_table, meta, search, paging)
+    print('-----------------------QUERY-----------------------')
+    print(query)
+    print(search.getRequests())
+    print('-----------------------QUERY-----------------------')
     cur.execute(query, search.getRequests())
     rows = cur.fetchall()
     return render_template("index.html",
@@ -74,9 +79,33 @@ def home():
         paging = paging,
         )
 
-@app.route("/modify/")
-def modif():
-    return 'memkek'
+@app.route("/modify/<string:selected_table>/<int:selected_id>/")
+def modifyPage(selected_table, selected_id):
+    if not selected_table in tables:
+        abort(404)
+    con = fdb.connect(
+        dsn=DB_PATH,
+        user='SYSDBA',
+        password='masterkey',
+        charset='UTF-8'
+    )
+    cur = con.cursor()
+    selected_table = getattr(metadata, selected_table)
+    meta = getMeta(selected_table)
+    query = QueryBuilder.getRowToModify(QueryBuilder(), selected_table, selected_id, meta)
+    cur.execute(query)
+    rows = cur.fetchall()
+    return render_template("modify.html",
+                           selected_id = selected_id,
+                           selected_table = selected_table,
+                           row = rows[0],
+                           meta = meta
+                           )
 
+@app.template_global()
+def modify(table, id, meta):
+    print(table)
+    print(meta)
+    print(id)
 
 app.run(debug=True)
--- query.py
+++ query.py
@@ -32,52 +32,64 @@ class Search:
 
 
 class QueryBuilder:
-    query = ''
 
-    def __init__(self, table, meta, search, paging):
-        self.createQuery(table, meta)
-        self.joinTable(table, meta)
-        self.addSearchRequest(table, search)
-        self.addSort(table, meta)
-        self.addPage(paging, search.getRequests())
+    def getTableView(self, table, meta, search, paging):
+        query = self.createQuery(table, meta)
+        query = query % self.addColsToSelect(table, meta)
+        query += self.joinTable(table, meta)
+        query += self.addSearchRequest(table, search)
+        query += self.addSort(table, meta)
+        query += self.addPage(paging, search.getRequests())
+        return query
 
     def createQuery(self, table, meta):
-        self.query = 'select %s from ' + table.tableName
+        query = 'select %s from ' + table.tableName
         self.countQuery = 'select count(*) from ' + table.tableName
-        return self.query
+        return query
+
+    def addColsToSelect(self, table, meta):
+        colsToSelect = []
+        for field in meta:
+            if isinstance(field, metadata.RefField):
+                colsToSelect.append(field.referenceTable.tableName + '.' + field.referenceCol.colName)
+            else:
+                colsToSelect.append(table.tableName + '.' + field.colName)
+        return ','.join(colsToSelect)
 
     def joinTable(self, table, meta):
-        colsToSelect = []
+        query = ''
         for field in meta:
             if isinstance(field, metadata.RefField):
                 temp = (
                     ' left join ' + field.referenceTable.tableName + ' on '  +
                     table.tableName + '.' + field.colName +  ' = ' +
                     field.referenceTable.tableName + '.' + field.referenceTable.id.colName + '\n')
-                self.query += temp
+                query += temp
                 self.countQuery += temp
-                colsToSelect.append(field.referenceTable.tableName + '.' + field.referenceCol.colName)
-            else:
-                colsToSelect.append(table.tableName + '.' + field.colName)
-        self.query = self.query % ','.join(colsToSelect)
-        return self.query
+        return query
 
     def addSearchRequest(self, table, search):
         request = []
+        query = ''
+        haveConditions = False
         for cond in search.conditions:
             if cond.request != '':
                 request.append('%s.%s %s ?')
                 if isinstance(cond.column, metadata.RefField):
                     request[-1] = request[-1] % (cond.column.referenceTable.tableName,
                                                  cond.column.referenceCol.colName, cond.operator)
-                if isinstance(cond.column, metadata.BaseField):
+                elif isinstance(cond.column, metadata.BaseField):
                     request[-1] = request[-1] % (table.tableName, cond.column.colName, cond.operator)
-                temp = ' where ' + ' and '.join(request)
-                self.countQuery += temp
-                self.query += temp
-        return self.query
+                haveConditions = True
+        if haveConditions:
+            temp = ' where ' + ' and '.join(request)
+            query += temp
+            self.countQuery += temp
+        return query
+
 
     def addSort(self, table, meta):
+        query = ''
         col = request.args.get('srt','')
         if col in table.__dict__:
             col = getattr(table, col)
@@ -92,16 +104,20 @@ class QueryBuilder:
                 tname = table.tableName
                 cname = col.colName
                 ctype = col.type
-            self.query += ' order by %s.%s'
+            query = ' order by %s.%s'
             if ctype == "reford":
-                self.query += ', %s.%s'
-                self.query = self.query % (tname, "order_number", tname, cname)
+                query += ', %s.%s'
+                query = query % (tname, "order_number", tname, cname)
             else:
-                self.query = self.query % (tname, cname)
-        return self.query
+                query = query % (tname, cname)
+        return query
 
     def addPage(self, paging, params):
+        query = ''
         paging.nextInit(self.countQuery, params)
-        self.query += ' offset %d rows fetch next %d rows only'
-        self.query = self.query % (paging.OnPage*paging.page, paging.OnPage)
-        return self.query
+        query += ' offset %d rows fetch next %d rows only'
+        query = query % (paging.onPage*paging.page, paging.onPage)
+        return query
+
+    def getRowToModify(self, table, id, meta):
+        pass
